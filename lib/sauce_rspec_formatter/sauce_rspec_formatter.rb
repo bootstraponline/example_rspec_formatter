d08ac0591a61ce07441a6fa457f284301434e0ec
bootstraponline <code@bootstraponline.com>
2015-10-08 21:53:36 -0400

    Add extra data by modifying full_description

--- lib/sauce_rspec_formatter/sauce_rspec_formatter.rb
+++ lib/sauce_rspec_formatter/sauce_rspec_formatter.rb
@@ -1,32 +1,20 @@
 require 'rspec/core/formatters/progress_formatter'
 
 class SauceProgressFormatter < RSpec::Core::Formatters::ProgressFormatter
-  RSpec::Core::Formatters.register self, :example_failed, :dump_failures, :dump_summary
+  RSpec::Core::Formatters.register self, :example_failed
 
   # Invoked when an example fails.
   #
   # @param failed_example_notification [RSpec::Core::Notifications::FailedExampleNotification]
   # containing example subclass of `RSpec::Core::Example`
   def example_failed(failed_example_notification)
-    # responsible for printing an 'F' to stdout
-    super
-  end
+     # todo: Dump example caps plus the sauce labs job link
+     # example.cap
+     # "https://saucelabs.com/beta/tests/#{example.session_id}"
 
-  # Dumps detailed information about each example failure.
-  #
-  # @param examples_notification [RSpec::Core::Notifications::ExamplesNotification]
-  def dump_failures(examples_notification)
-    # responsible for 'Failures:' detailed info
-    super
-  end
+     failed_example_notification.example.metadata[:full_description] += ' - added description 2!'
 
-  # This method is invoked after the dumping of examples and failures.
-  # Each parameter is assigned to a corresponding attribute.
-  #
-  # @param summary [SummaryNotification] containing duration,
-  #   example_count, failure_count and pending_count
-  def dump_summary(summary)
-    # responsible for 'Failed examples:' summary
+    # prints an 'F' to stdout
     super
   end
 end
@@ -35,18 +23,4 @@ end
 https://github.com/rspec/rspec-core/blob/7477810fc65718a69753f04d4a99c3acf2371acb/lib/rspec/core/formatters/protocol.rb
 https://github.com/rspec/rspec-core/blob/7477810fc65718a69753f04d4a99c3acf2371acb/lib/rspec/core/formatters/progress_formatter.rb
 https://github.com/saucelabs/sauce_ruby/blob/master/lib/sauce/rspec/rspec_formatter.rb
-
---
-
-Dump example caps plus the sauce labs job link
-
-example.cap
-
-"https://saucelabs.com/beta/tests/#{example.session_id}"
-
---
-
-SauceOnDemandSessionID=11111122222233334444 job-name=Sign in page loads successfully
-  loads successfully
-
 =end
